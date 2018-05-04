0b4cfb3dd361497ffee88cce4eec2bc3c0d83ddd
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-05-04 14:54:01 +0900

    AVFormatContext

--- src/fplayer.cpp
+++ src/fplayer.cpp
@@ -2,16 +2,35 @@ extern "C" {
   #include <libavformat/avformat.h>
 }
 
-#include <iostream>
-
 #pragma comment(lib, "avformat.lib")
 #pragma comment(lib, "avutil.lib")
 
-using namespace std;
-
 int main(void)
 {
+  const char *szFilePath = "OneLINE.avi";
+  int ret;
+
   av_register_all();
-  av_log(NULL, AV_LOG_INFO, "Hello FFMpeg\n");
+  avformat_network_init();
+
+  AVFormatContext *pFmtCtx = NULL;
+
+  ret = avformat_open_input(&pFmtCtx, szFilePath, NULL, NULL);
+  if (ret != 0) {
+    av_log(NULL, AV_LOG_ERROR, "File [%s] Open Fail (ret: %d)\n", szFilePath, ret);
+    exit(-1);
+  }
+  av_log(NULL, AV_LOG_INFO, "File [%s] Open Success\n", szFilePath);
+  av_log(NULL, AV_LOG_INFO, "Format: %s\n", pFmtCtx->iformat->name);
+
+  ret = avformat_find_stream_info(pFmtCtx, NULL);
+  if (ret < 0) {
+		av_log(NULL, AV_LOG_ERROR, "Fail to get Stream Information\n");
+		exit(-1);
+	}
+	av_log(NULL, AV_LOG_INFO, "Get Stream Information Success\n");
+
+  avformat_close_input(&pFmtCtx);
+  avformat_network_deinit();
   return 0;
 }
