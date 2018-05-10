64539e79b2c4bfe37513c11a01f9db64d17e9104
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-05-11 00:07:40 +0900

    sdl window

--- src/sdlview.cpp
+++ src/sdlview.cpp
@@ -1,14 +1,43 @@
-#include <iostream>
+#include <cstdio>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_thread.h>
 
 int main (int argc, char *argv[])
 {
-  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-    std::cout << "SDL could not initialize. SDL error: " << SDL_GetError() << std::endl;
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0) {
+    fprintf(stderr, "could not initialize SDL - %s\n", SDL_GetError());
+    return EXIT_FAILURE;
   }
 
-  system("pause");
+  SDL_Window *window = SDL_CreateWindow("SDL_CreateTexture", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 640, 480);
+  SDL_Rect r;
+  SDL_Event event;
+
+  r.w = 100;
+  r.h = 50;
+
+  while (1) {
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT)
+      break;
+    r.x = rand()%500;
+    r.y = rand()%500;
+
+    SDL_SetRenderTarget(renderer, texture);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+    SDL_RenderDrawRect(renderer,&r);
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
+    SDL_RenderFillRect(renderer, &r);
+    SDL_SetRenderTarget(renderer, NULL);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+  }
   
+  SDL_DestroyRenderer(renderer);
+  SDL_Quit();
   return EXIT_SUCCESS;
 }
