0ea77603eab5996dbaafa7deb92db97809b91c76
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-05-15 11:46:55 +0900

    thread

--- src/fplayerT3.cpp
+++ src/fplayerT3.cpp
@@ -1,6 +1,9 @@
 extern "C" {
   #include <libavformat/avformat.h>
+  #include <libswscale/swscale.h>
+  #include <libswresample/swresample.h>
   #include <libavutil/avutil.h>
+  #include <libavutil/avstring.h>
 }
 
 #include <SDL2/SDL.h>
@@ -8,12 +11,17 @@ extern "C" {
 #define FF_REFRESH_EVENT  (SDL_USEREVENT + 1)
 #define FF_QUIT_EVENT     (SDL_USEREVENT + 2)
 
+#define VIDEO_PICTURE_QUEUE_SIZE (1)
+
 typedef struct _PacketQueue
 {
 } PacketQueue;
 
 typedef struct _VideoPicture
 {	
+  SDL_Texture *texture;
+  int width, height;
+  int allocated;
 } VideoPicture;
 
 typedef struct _VideoState {
@@ -21,10 +29,21 @@ typedef struct _VideoState {
   AVCodecContext *audioCtx;
   AVCodecContext *videoCtx;
 
+  struct SwrContext *pSwrCtx;
+  struct SwsContext *pSwsCtx;
+
   int video_index, audio_index;
+
+  AVStream *audio_stream;
+  AVStream *video_stream;
+
+  AVFrame *pFrameRGB;
 
   PacketQueue audioq;
   PacketQueue videoq;
+
+  VideoPicture pictq[VIDEO_PICTURE_QUEUE_SIZE];
+  int pictq_size, pictq_rindex, pictq_windex;
 
   SDL_mutex *pictq_mutex;
   SDL_cond *pictq_cond;
@@ -32,8 +51,14 @@ typedef struct _VideoState {
   SDL_Thread *parse_tid;
   SDL_Thread *video_tid;
 
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+
+  char filename[1024];
   int quit;
 } VideoState;
+
+SDL_mutex *screen_mutex;
 
 void PacketQueueInit(PacketQueue *q) {
 }
@@ -62,8 +87,54 @@ void schedule_refresh(VideoState *is, int delay) {
 void video_refresh_timer(void *userdata) {
 }
 
-int queue_picture(VideoState *is, AVFrame *frame) {
+void alloc_picture(void *userdata) {
+  VideoState *is = static_cast<VideoState *>(userdata);
+  VideoPicture *vp;
 
+  vp = &is->pictq[is->pictq_windex];
+  if (vp->texture) {
+    SDL_DestroyTexture(vp->texture);
+  }
+  SDL_LockMutex(screen_mutex);
+  vp->texture = SDL_CreateTexture(is->renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, is->videoCtx->width, is->videoCtx->height);
+  SDL_UnlockMutex(screen_mutex);
+  vp->width = is->videoCtx->width;
+  vp->height = is->videoCtx->height;
+  vp->allocated = 1;
+}
+
+int queue_picture(VideoState *is, AVFrame *frame) {
+  VideoPicture *vp;
+
+  SDL_LockMutex(is->pictq_mutex);
+  while (is->pictq_size >= VIDEO_PICTURE_QUEUE_SIZE && !is->quit) {
+    SDL_CondWait(is->pictq_cond, is->pictq_mutex);
+  }
+  SDL_UnlockMutex(is->pictq_mutex);
+
+  // windex is set to 0 initially
+  vp = &is->pictq[is->pictq_windex];
+
+  // allocate or resize the buffer
+  if (!vp->texture || vp->width != is->videoCtx->width || vp->height != is->videoCtx->height) {
+    vp->allocated = 0;
+    alloc_picture(is);
+  }
+
+  if (vp->texture) {
+    sws_scale(is->pSwsCtx, frame->data, frame->linesize, 0, is->videoCtx->height, is->pFrameRGB->data, is->pFrameRGB->linesize);
+    SDL_UpdateTexture(vp->texture, NULL, is->pFrameRGB->data[0], is->pFrameRGB->linesize[0]);
+
+    if (++is->pictq_windex == VIDEO_PICTURE_QUEUE_SIZE) {
+      is->pictq_windex = 0;
+    }
+
+    SDL_LockMutex(is->pictq_mutex);
+    is->pictq_size++;
+    SDL_UnlockMutex(is->pictq_mutex);
+  }
+
+  return 0;
 }
 
 int video_thread(void *arg) {
@@ -115,6 +186,13 @@ int stream_component_open(VideoState *is, int stream_index) {
     SDL_PauseAudio(0);
   }
   else if (codecCtx->codec_type == AVMEDIA_TYPE_VIDEO) {
+    is->videoCtx = codecCtx;
+    is->video_index = stream_index;
+    is->video_stream = pFormatCtx->streams[stream_index];
+    is->pSwsCtx = sws_getContext(codecCtx->width, codecCtx->height, codecCtx->pix_fmt, codecCtx->width, codecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+
+    is->window = SDL_CreateWindow(is->filename, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, codecpar->width, codecpar->height, 0);
+    is->renderer = SDL_CreateRenderer(is->window, -1, 0);
     is->video_tid = SDL_CreateThread(video_thread, "video", is);
   }
 
@@ -158,7 +236,13 @@ fail:
 int main(int argc, char *argv[]) {
   SDL_Event event;
   
+  const char *filename = "file:little.mkv";
+  
   VideoState *is = static_cast<VideoState *>(av_mallocz(sizeof(VideoState)));
+
+  screen_mutex = SDL_CreateMutex();
+
+  av_strlcpy(is->filename, filename, sizeof(is->filename));
 
   is->pictq_mutex = SDL_CreateMutex();
   is->pictq_cond = SDL_CreateCond();
